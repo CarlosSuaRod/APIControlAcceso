a798c4dc622ae070a5bddbee97ccafe60d41e23b
CarlosSuaRod <suarezcarlos641@gmail.com>
2023-03-16 09:35:40 +0000

    funsiona

--- src/main/java/com/mycompany/apicontrolacceso/PuertasController.java
+++ src/main/java/com/mycompany/apicontrolacceso/PuertasController.java
@@ -13,7 +13,9 @@ import java.util.ArrayList;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PuertasController {
     private Connection con=new MiConexion().getConnection();
     
